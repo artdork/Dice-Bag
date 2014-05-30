6bc763bcdda9db64bad07d42ad33264f277c1842
Kristin Henson <kristin.b.henson@gmail.com>
2014-05-29 23:38:37 -0400

    Theme and animation changes
    -Updated theme to look more modern and sleek.
    -Made dice rolling animation more random, so it now spins to anywhere
    on the screen.
    -Added the basic structure for a second page to control settings.

--- includes/script.js
+++ includes/script.js
@@ -1,20 +1,28 @@
 // JavaScript Document
 
+function giveMeRandom(maxNum, floor) {
+	return (Math.floor(Math.random() * maxNum) + floor);
+}
 
 function rollDie() {
-	$('.die').html('<img src="images/6sideddie' + (Math.floor(Math.random()*6)+1) + '.png"> '); 
+	$('.die').html('<img src="images/6sideddie' + giveMeRandom(6, 1) + '.png"> '); 
 };
 
+    	
 
 //Moves image to the screen 
 $(document).ready(function() {
   $('.btn1').click(function() {
     $('.die').removeAttr('style');
+	$('.die').removeClass('diespin');
 	$('.die').animate({ 
-       top: '-360px',
-       left: '50px',
-     }, 500);
-	$('.die').addClass('diespin');
+       bottom: giveMeRandom(275, 60) + 'px',
+       left: giveMeRandom(190, -100) + 'px',
+     }, 400);
+	$('.die').addClass('rot').rotate({
+            duration: 400,
+            angle: 0,
+            animateTo: giveMeRandom(180, 1350)});
   })
 
 });
